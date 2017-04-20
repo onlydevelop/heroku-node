f035087c82a3011d0c5a03f410ef9f35c17fadf0
Dipanjan Bhowmik <onlydevelop@gmail.com>
2017-04-20 23:27:07 +0530

    Added a new /books uri

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', function (req, res) {
    res.send('It works perfectly in Heroku!');
 })
 
+app.get('/books', function (req, res) {
+   res.send('Shows list of books!');
+})
+
 var server = app.listen(process.env.PORT || PORT, function () {
    var port = server.address().port
    console.log("Listening at port: %s", port)
