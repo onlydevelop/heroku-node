f1a24514d08f01d53abf9bff86545f95be88c4b5
Dipanjan Bhowmik <onlydevelop@gmail.com>
2017-04-20 23:17:21 +0530

    Updated the port

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
 
-var PORT = 8080;
+var PORT = 3000;
 
 app.get('/', function (req, res) {
    res.send('It works perfectly in Heroku!');
 })
 
-var server = app.listen(PORT, function () {
+var server = app.listen(process.env.PORT || PORT, function () {
    var port = server.address().port
-   console.log("Listening at port: %s", PORT)
+   console.log("Listening at port: %s", port)
 })
